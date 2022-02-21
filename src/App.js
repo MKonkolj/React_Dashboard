2bcbb79a72579445ea18b4e03b5e215fdf571274
Mladen Konkolj <mladenkonkolj@gmail.com>
2022-02-21 13:43:16 +0100

    postavljen sidebar i sređen css za njega

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import Sidebar from "./Components/Sidebar";
+
 function App() {
   return (
-    <div className="App">
-      
+    <div className="App grid-2-1">
+      <Sidebar />
+      <div className="dashboard-container">
+        <div className="dashboard">
+          <Dashboard />
+        </div>
+      </div>
     </div>
   );
 }
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,17 +1,17 @@
 function Sidebar() {
   return (
     <div className="sidebar">
-        <img src="img/universal-logo.png" alt="universal logo"/>
-        <ul class="sidebar-menu" id="sidebarMenu">
-            <li class="current"><Link to= "">Users</Link></li>
-            <li><Link to= "">Developers</Link></li>
-            <li><Link to= "">Clients</Link></li>
-            <li><Link to= "">My profile</Link></li>
+        <img src="./images/universal-logo-small.png" alt="universal logo"/>
+        <ul className="sidebar-menu" id="sidebarMenu">
+            <li className="current"><a>Users</a></li>
+            <li><a>Developers</a></li>
+            <li><a>Clients</a></li>
+            <li><a>My profile</a></li>
         </ul>
-        <div class="burger" id="burger">
-            <div class="bar bar1"></div>
-            <div class="bar bar2"></div>
-            <div class="bar bar3"></div>
+        <div className="burger" id="burger">
+            <div className="bar bar1"></div>
+            <div className="bar bar2"></div>
+            <div className="bar bar3"></div>
         </div>
     </div>
   )
