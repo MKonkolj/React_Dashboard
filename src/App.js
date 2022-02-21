a5ece18d39222517cbcd9e8b62dddd5f691ba3f5
Mladen Konkolj <mladenkonkolj@gmail.com>
2022-02-21 13:58:21 +0100

    postavljeni router linkovi

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import Sidebar from "./Components/Sidebar";
+import Users from "./Pages/Users";
+import Developers from "./Pages/Developers";
+import Clients from "./Pages/Clients";
+import MyProfile from "./Pages/MyProfile";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App grid-2-1">
+      {/* <BrowserRouter>
+      <Sidebar />
+        <Routes>
+          <Route path="/" element={<Browse />} />
+          <Route path="/myreviews" element={<MyReviews />} />
+          <Route path="/mygames" element={<MyGames />} />
+          <Route path="/newreview" element={<NewReview />} />
+          <Route path="/game/:id" element={<GamePage />} />
+        </Routes>
+      </BrowserRouter> */}
+      <BrowserRouter>
       <Sidebar />
       <div className="dashboard-container">
         <div className="dashboard">
-          <Dashboard />
+      <Routes>
+        <Route path="/" element={<Users />} />
+        <Route path="/developers" element={<Developers />} />
+        <Route path="/clients" element={<Clients />} />
+        <Route path="/my-profile" element={<MyProfile />} />
+      </Routes>
         </div>
       </div>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,12 +1,14 @@
+import { Link } from "react-router-dom"
+
 function Sidebar() {
   return (
     <div className="sidebar">
         <img src="./images/universal-logo-small.png" alt="universal logo"/>
         <ul className="sidebar-menu" id="sidebarMenu">
-            <li className="current"><a>Users</a></li>
-            <li><a>Developers</a></li>
-            <li><a>Clients</a></li>
-            <li><a>My profile</a></li>
+            <li className="current"><Link to="/">Users</Link></li>
+            <li><Link to="/developers">Developers</Link></li>
+            <li><Link to="/clients">Clients</Link></li>
+            <li><Link to="/my-profile">My profile</Link></li>
         </ul>
         <div className="burger" id="burger">
             <div className="bar bar1"></div>
