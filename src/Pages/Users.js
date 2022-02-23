11c0269647a8d5726fad3e298977809e2355136c
Mladen Konkolj <mladenkonkolj@gmail.com>
2022-02-23 14:10:33 +0100

    napravljena table komponenta

--- src/Pages/Users.js
+++ src/Pages/Users.js
@@ -1,24 +1,28 @@
-import useFetch from "../Hooks/useFetch"
 import Searchbar from "../Components/Searchbar"
+import Table from "../Components/Table";
+import useFetch from "../Hooks/useFetch";
 import { useEffect, useState } from "react";
 
 function Users() {
 
+  // Get all users from db
   const { data: users, isLoading, error } = useFetch("http://localhost:8000/users")
 
+  // Set all users initially to be displayed
   const [searchedUsers, setSearchedUsers] = useState();
 
   useEffect(() => {
       setSearchedUsers(users);
-    }, [users])
+      }, [users])
 
+  // Filter users by searchbox value
   function handleOnChange (e) {
-    let searchQuery = e.target.value.toLowerCase();
-    setSearchedUsers(users.filter(user => 
+      let searchQuery = e.target.value.toLowerCase();
+      setSearchedUsers(users.filter(user => 
       user.first_name.toLowerCase().includes(searchQuery)
       || user.last_name.toLowerCase().includes(searchQuery)
       || user.email.toLowerCase().includes(searchQuery)
-    ))
+      ))
   }
 
   return (
@@ -26,43 +30,26 @@ function Users() {
       {/* SEARCHBAR */}
       <Searchbar handleOnChange={handleOnChange}/>
       {/* TABLE */}
-      <div className="table-div">
-        <table className="table">
-          <thead>
-            <tr>
-            <th className="table-cell avatar-column">Avatar</th>
-            <th className="table-cell name-column">First name</th>
-            <th className="table-cell surname-column">Last name</th>
-            <th className="table-cell city-column">City</th>
-            <th className="table-cell role-column">Role</th>
-            <th className="table-cell status-column">Status</th>
-            <th className="table-cell email-column">Email</th>
-            <th className="table-cell options-column">Options</th>
-            </tr>
-          </thead>
-          <tbody>
-            {searchedUsers && searchedUsers.map((user, index) => {
-              return(
-              <tr key={index}>
-              <td className="table-cell avatar-column"><img src={user.avatar.image_path} alt={user.avatar.image_alt}/></td>
-              <td className="table-cell name-column">{user.first_name}</td>
-              <td className="table-cell surname-column">{user.last_name}</td>
-              <td className="table-cell city-column">{user.city}</td>
-              <td className="table-cell role-column">{user.role}</td>
-              <td className="table-cell status-column">{user.status}</td>
-              <td className="table-cell email-column">{user.email}</td>
-              <td className="table-cell options-column">
-                <div className="options-container">
-                  <span className="edit-option options-icon" id="editBtn"></span>
-                  <a><span className="view-option options-icon"></span></a>
-                  <span className="delete-option options-icon" id="deleteBtn"></span>
-                </div>
-              </td>
-            </tr>
-            )})}
-          </tbody>
-        </table>
-      </div>
+      <Table
+        data={searchedUsers}
+        columns={{
+          avatar: true,
+          first_name: true,
+          lastName: true,
+          city: true,
+          country: false,
+          role: true,
+          status: true,
+          email: true
+        }}
+        options={{
+          time: false,
+          edit: true,
+          view: true,
+          invoice: false,
+          delete: true
+        }}
+      />
       {/* TABLE END */}
     </div>
   )
