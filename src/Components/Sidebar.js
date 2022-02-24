bcfd5b05fd32552021d47d2f7d1c714fc0baf4ab
Mladen Konkolj <mladenkonkolj@gmail.com>
2022-02-24 16:00:08 +0100

    dodat navlink

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,14 +1,14 @@
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 function Sidebar() {
   return (
     <div className="sidebar">
         <img src="./images/universal-logo-small.png" alt="universal logo"/>
         <ul className="sidebar-menu" id="sidebarMenu">
-            <li className="current"><Link to="/users">Users</Link></li>
-            <li><Link to="/developers">Developers</Link></li>
-            <li><Link to="/clients">Clients</Link></li>
-            <li><Link to="/my-profile">My profile</Link></li>
+            <li><NavLink to="/users">Users</NavLink></li>
+            <li><NavLink to="/developers">Developers</NavLink></li>
+            <li><NavLink to="/clients">Clients</NavLink></li>
+            <li><NavLink to="/my-profile">My profile</NavLink></li>
         </ul>
         <div className="burger" id="burger">
             <div className="bar bar1"></div>
